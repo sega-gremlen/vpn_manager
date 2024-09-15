11d86a6c8b4312d6f588b62815cbd9b03e763d66
fila <doartlos@gmail.com>
2024-09-15 23:26:02 +0400

    aps fix try

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from app.bot.utils.commands import set_commands
 
 async def start():
     try:
+        logging.basicConfig(level=logging.INFO)
         await dp.start_polling(bot)
         await set_commands(bot)
     finally:
